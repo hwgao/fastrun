8018ae99071905e8fe4457879c4495743fe0a023
hongwei gao <hwgao@hwgao-VF14.(none)>
2012-09-07 09:33:10 +0800

    add if file exists check

--- dm.sh
+++ dm.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-# make sure the dirctory list file is existed
-touch ~/.dm
 
 # save the current directory into directory list
 function ds()
 {
 	s2=' #*'
 
+	# make sure the dirctory list file is existed
+	if [ ! -e ~/.dm ]; then
+		touch ~/.dm
+	fi
+
 	count=1
 	while read LINE
 	do
-		if [[ ${LINE%$s2} == `pwd` ]]; then
+		if [ ${LINE%%$s2} == `pwd` ]; then
 			return
 		fi
 		let count++
 	done < ~/.dm
 
 	echo -n `pwd` >> ~/.dm
-	if [[ $# -eq 1 ]]; then
+	if [ $# -eq 1 ]; then
 		echo " #$1" >> ~/.dm
 	else
 		echo " #$count" >> ~/.dm
@@ -28,19 +31,28 @@ function ds()
 # view the directory list in vi, and you can edit it
 function dv()
 {
-	vi ~/.dm
+	if [ -e ~/.dm ] && [ -s ~/.dm ]; then
+		vi ~/.dm
+	else  
+		echo "Dirctory list is empty"
+	fi
 }
 
 #show the directory list, and change to the selected directory
 function dm()
 {
+	if [ ! -e ~/.dm ] || [ ! -s ~/.dm ]; then
+		echo "Dirctory list is empty"
+		return
+	fi
+
 	s1='/* #'
 	s2=' #*'
 
-	if [[ $# -eq 1 ]]; then
+	if [ $# -eq 1 ]; then
 		while read LINE
 		do
-			if [[ ${LINE##$s1} -eq $1 ]]; then
+			if [ ${LINE#$s1} -eq $1 ]; then
 				echo "Change to ${LINE%%$s2}"
 				cd $LINE
 				return
@@ -57,7 +69,7 @@ function dm()
 	while read LINE
 	do
 		let count++
-		if [[ $count -eq $number ]]; then
+		if [ $count -eq $number ]; then
 			echo "Change to ${LINE%%$s2}"
 			cd $LINE
 			break
